2cc041bbfda602ab91dba615d2cb52b8b398d106
Thomas Nipen (freiburg) <tnipen@gmail.com>
2013-10-10 21:18:49 +0200

    Forecast now reuses Entities date, offset, etc

--- src/Forecast.cpp
+++ src/Forecast.cpp
@@ -1,8 +1,19 @@
 #include "Forecast.h"
 
 Forecast::Forecast(const Deterministic& iDeterministic, const Ensemble& iEnsemble, const Distribution::ptr iDistribution) :
+   Entity(iEnsemble.getDate(), iEnsemble.getInit(), iEnsemble.getOffset(), iEnsemble.getLocation(), iEnsemble.getVariable()), 
       mDeterministic(iDeterministic),
       mEnsemble(iEnsemble),
       mDistribution(iDistribution)
 {
+   assert(mDeterministic.getDate() == mEnsemble.getDate());
+   assert(mDeterministic.getInit() == mEnsemble.getInit());
+   assert(mDeterministic.getOffset() == mEnsemble.getOffset());
+   assert(mDeterministic.getLocation().getId() == mEnsemble.getLocation().getId());
+   assert(mDeterministic.getVariable() == mEnsemble.getVariable());
+   assert(mDistribution->getDate() == mEnsemble.getDate());
+   assert(mDistribution->getInit() == mEnsemble.getInit());
+   assert(mDistribution->getOffset() == mEnsemble.getOffset());
+   assert(mDistribution->getLocation().getId() == mEnsemble.getLocation().getId());
+   assert(mDistribution->getVariable() == mEnsemble.getVariable());
 }
--- src/Forecast.h
+++ src/Forecast.h
@@ -10,23 +10,13 @@ class Forecast : public Entity {
       Forecast(const Deterministic& iDeterministic, const Ensemble& iEnsemble, const Distribution::ptr iDistribution);
       // This should be done by configuration
       // Forecast(int iDate, int iInit, float iOffset, const Location& iLocation, std::string iVariable, const Configuration& iConfiguration);
-      Deterministic getDeterministic() const {return mDeterministic;};
-      Ensemble getEnsemble() const {return mEnsemble;};
+      Deterministic     getDeterministic() const {return mDeterministic;};
+      Ensemble          getEnsemble() const {return mEnsemble;};
       Distribution::ptr getDistribution() const {return mDistribution;};
-      int getDate() const {return mEnsemble.getDate();}; 
-      Location getLocation() const {return mEnsemble.getLocation();};
-      float getOffset() const {return mEnsemble.getOffset();};
-      int getInit() const {return mEnsemble.getInit();};
-      std::string getVariable() const {return mEnsemble.getVariable();};
    private:
-      Deterministic mDeterministic;
-      Ensemble mEnsemble;
+      Deterministic     mDeterministic;
+      Ensemble          mEnsemble;
       Distribution::ptr mDistribution;
-      int mDate;
-      float mOffset;
-      int mInit;
-      Location mLocation;
-      std::string mVariable;
 };
 #endif
 
