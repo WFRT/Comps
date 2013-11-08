07f2eae10702350ab210f9fade47da6f4c2dbd2e
Thomas Nipen <tnipen@gmail.com>
2013-11-08 23:01:44 +0100

    Fixes bug in DiscreteFixed
    Previously a probability of 0 was always returned. Now the scheme
    returns the fixed probability passed through the options.

--- src/Discretes/Fixed.cpp
+++ src/Discretes/Fixed.cpp
@@ -3,4 +3,9 @@
 DiscreteFixed::DiscreteFixed(const Options& iOptions, const Data& iData) : Discrete(iOptions, iData) {
    //! Value of the fixed probability
    iOptions.getRequiredValue("value", mValue);
+   if(mValue < 0 || mValue > 1) {
+      std::stringstream ss;
+      ss << "DiscreteFixed: probability mass 'value', must be between 0 and 1";
+      Global::logger->write(ss.str(), Logger::error);
+   }
 }
--- src/Discretes/Fixed.h
+++ src/Discretes/Fixed.h
@@ -7,7 +7,7 @@ class Distribution;
 class DiscreteFixed : public Discrete {
    public:
       DiscreteFixed(const Options& iOptions, const Data& iData);
-      float getPCore(const Ensemble& iEnsemble, const Parameters& iParameters) const {return 0;};
+      float getPCore(const Ensemble& iEnsemble, const Parameters& iParameters) const {return mValue;};
       bool isMemberDependent() const {return false;};
       bool isLocationDependent() const {return false;};
       bool needsTraining() const {return false;};
