8c99072710ccc573f38e712e0b563c9d7e35921f
Thomas Nipen <tnipen@gmail.com>
2013-11-09 16:48:25 +0100

    Fixes bug in QcDefault for variables with one discrete point

--- src/Qcs/Default.cpp
+++ src/Qcs/Default.cpp
@@ -9,13 +9,12 @@ bool QcDefault::checkCore(const Value& iValue) const {
    const Variable* var = Variable::get(iValue.getVariable());
    float value = iValue.getValue();
 
-   if(Global::isValid(var->getMin()) && Global::isValid(var->getMax())) {
-      if(value < var->getMin() || value > var->getMax()) {
-         std::stringstream ss;
-         ss << var->getName() << " value of " << value << " on " << iValue.getDate() << " " << iValue.getOffset() << " assumed missing";
-         Global::logger->write(ss.str(), Logger::warning);
-         return false;
-      }
+   if((Global::isValid(var->getMin()) && value < var->getMin()) ||
+      (Global::isValid(var->getMax()) && value > var->getMax())) {
+      std::stringstream ss;
+      ss << var->getName() << " value of " << value << " on " << iValue.getDate() << " " << iValue.getOffset() << " assumed missing";
+      Global::logger->write(ss.str(), Logger::warning);
+      return false;
    }
    return true;
 }
