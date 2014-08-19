b23c83a57922fbcdc67dc4fb7ca2a6f6a0b8f7c3
Thomas Nipen (freiburg) <tnipen@gmail.com>
2014-08-19 11:04:54 -0600

    Show counts for each location in info.exe

--- src/Drivers/Info.cpp
+++ src/Drivers/Info.cpp
@@ -11,12 +11,14 @@
 #include "../Member.h"
 #include <cstring>
 bool mShowAll;
+bool mShowStat;
 bool mShowSpecific;
 void showLocations(Input* input);
 void showOffsets(Input* input);
 void showDates(Input* input);
 void showMembers(Input* input);
 void showVariables(Input* input);
+int getNumValid(Input* input, std::string variable, int locationId, int startDate, int endDate);
 
 int main(int argc, const char *argv[]) {
    Global::setLogger(new LoggerDefault(Logger::message));
@@ -24,8 +26,9 @@ int main(int argc, const char *argv[]) {
    if(argc != 2 && argc != 3 && argc != 4) {
       std::cout << "Show information about a COMPS dataset" << std::endl;
       std::cout << std::endl;
-      std::cout << "usage: info.exe dataset [--show-all]    show information about dataset" << std::endl;
-      std::cout << "   or: info.exe dataset date            show detailed information for a specific date" << std::endl;
+      std::cout << "usage: info.exe dataset [--show-all]       show information about dataset" << std::endl;
+      std::cout << "   or: info.exe dataset date               show detailed information for a specific date" << std::endl;
+      std::cout << "   or: info.exe dataset startDate endDate  show detailed information for a date range" << std::endl;
       std::cout << std::endl;
       std::cout << "Arguments:" << std::endl;
       std::cout << "   dataset         Tag of dataset from namelist" << std::endl;
@@ -37,19 +40,30 @@ int main(int argc, const char *argv[]) {
 
    mShowAll = false;
    mShowSpecific = false;
-   int date = 0;
+   int startDate = Global::MV;
+   int endDate   = Global::MV;
 
    std::string dataset(argv[1]);
    if(argc >= 3) {
-      if(!std::strcmp(argv[2], "--show-all")) {
-         mShowAll = true;
+      for(int i = 2; i < argc; i++) {
+         if(!std::strcmp(argv[i], "--show-all")) {
+            mShowAll = true;
+         }
+         else if(!Global::isValid(startDate)) {
+            std::stringstream ss;
+            ss<< std::string(argv[i]);
+            ss >> startDate;
+            endDate = startDate;
+         }
+         else {
+            std::stringstream ss;
+            ss<< std::string(argv[i]);
+            ss >> endDate;
+         }
       }
-      else {
-         std::stringstream ss;
-         ss<< std::string(argv[2]);
-         ss >> date;
-         mShowSpecific = true;
-      }
+   }
+   if(Global::isValid(startDate)) {
+      mShowSpecific = true;
    }
 
    InputContainer inputContainer(Options(""));
@@ -70,18 +84,22 @@ int main(int argc, const char *argv[]) {
       std::cout << "Variable     min   mean  max        count (max "
                 << offsets.size()*locations.size()*members.size() << ")" << std::endl;
       for(int v = 0; v < variables.size(); v++) {
-         for(int i = 0; i < offsets.size(); i++) {
-            for(int k = 0; k < locations.size(); k++) {
-               for(int m = 0; m < members.size(); m++) {
-                  float value = input->getValue(date, 0, offsets[i], locations[k].getId(), members[m].getId(), variables[v]);
-                  if(Global::isValid(value)) {
-                     min[v] = std::min(min[v], value);
-                     mean[v] += value;
-                     max[v] = std::max(max[v], value);
-                     num[v]++;
+         int date = startDate;
+         while(date <= endDate) {
+            for(int i = 0; i < offsets.size(); i++) {
+               for(int k = 0; k < locations.size(); k++) {
+                  for(int m = 0; m < members.size(); m++) {
+                     float value = input->getValue(date, 0, offsets[i], locations[k].getId(), members[m].getId(), variables[v]);
+                     if(Global::isValid(value)) {
+                        min[v] = std::min(min[v], value);
+                        mean[v] += value;
+                        max[v] = std::max(max[v], value);
+                        num[v]++;
+                     }
                   }
                }
             }
+            date = Global::getDate(date, 24);
          }
          if(num[v] > 0)
             mean[v] /= num[v];
@@ -95,6 +113,35 @@ int main(int argc, const char *argv[]) {
                    << std::setprecision(3) << std::setw(5) << mean[v]<< " "
                    << std::setprecision(3) << std::setw(10) << max[v] << " " << num[v] << std::endl;
       }
+
+      // Show results for each location
+      std::cout << std::endl;
+      std::cout << "Counts for each location and variable" << std::endl;
+      std::cout << "Id    ";
+      for(int v = 0; v < variables.size(); v++) {
+         std::cout << std::setw(12) << variables[v];
+      }
+      std::cout << std::endl;
+      for(int k = 0; k < locations.size(); k++) {
+         int num = 0;
+         std::cout << std::setw(6) << locations[k].getId();
+         for(int v = 0; v < variables.size(); v++) {
+            int date = startDate;
+            while(date <= endDate) {
+               for(int i = 0; i < offsets.size(); i++) {
+                  for(int m = 0; m < members.size(); m++) {
+                     float value = input->getValue(startDate, 0, offsets[i], locations[k].getId(), members[m].getId(), variables[v]);
+                     if(Global::isValid(value)) {
+                        num++;
+                     }
+                  }
+               }
+               date = Global::getDate(date, 24);
+            }
+            std::cout << std::setw(12) << num;
+         }
+         std::cout << std::endl;
+      }
    }
    else {
       // Show general statistics
@@ -105,19 +152,29 @@ int main(int argc, const char *argv[]) {
       showMembers(input);
    }
 }
+
 void showLocations(Input* input) {
    const std::vector<Location>& locations = input->getLocations();
    std::cout << "Locations (" << locations.size() << " total):" << std::endl;
-   std::cout << "Id       code   lat     lon   elev gradient" << std::endl;
+   std::cout << "Id       code   lat     lon   elev gradient";
+   std::cout << std::endl;
    for(int i = 0; i < locations.size(); i++) {
       if(mShowAll || locations.size() < 100 || i < 5 || i >= locations.size()-5) {
          std::cout << std::setw(6) << locations[i].getId() << " "
                    << std::setw(6) << locations[i].getCode() << " " 
                    << std::setw(6) << std::fixed << std::setprecision(1) << locations[i].getLat() << " "
                    << std::setw(6) << locations[i].getLon() << " "
-                   << std::setw(6) << std::fixed << std::setprecision(1) << locations[i].getElev() << " "
-                   << std::setw(6) << std::fixed << std::setprecision(3) << locations[i].getGradientX() << ","
-                   << std::setw(6) << std::fixed << std::setprecision(3) << locations[i].getGradientY() << std::endl;
+                   << std::setw(6) << std::fixed << std::setprecision(1) << locations[i].getElev() << " ";
+         float gradX = locations[i].getGradientX();
+         float gradY = locations[i].getGradientY();
+         if(Global::isValid(gradX) && Global::isValid(gradY)) {
+            std::cout << std::setw(6) << std::fixed << std::setprecision(3) << gradX << ","
+                      << std::setw(6) << std::fixed << std::setprecision(3) << gradY;
+         }
+         else {
+            std::cout << "            ";
+         }
+         std::cout << std::endl;
       }
       else if(i == 5) {
          std::cout << "..." << std::endl;
@@ -188,4 +245,32 @@ void showMembers(Input* input) {
       }
    }
    std::cout << std::endl;
+}
+
+int getNumValid(Input* input, std::string variable, int locationId, int startDate, int endDate) {
+   std::vector<float> offsets = input->getOffsets();
+   std::vector<Location> locations = input->getLocations();
+   std::vector<Member> members = input->getMembers();
+
+   // float min=0,mean=0,max=0,num=0;
+   float num = 0;
+
+   int date = startDate;
+   while(date <= endDate) {
+      for(int i = 0; i < offsets.size(); i++) {
+         for(int k = 0; k < locations.size(); k++) {
+            for(int m = 0; m < members.size(); m++) {
+               float value = input->getValue(date, 0, offsets[i], locationId, members[m].getId(), variable);
+               if(Global::isValid(value)) {
+                  // min = std::min(min, value);
+                  // mean += value;
+                  // max = std::max(max, value);
+                  num++;
+               }
+            }
+         }
+      }
+      date = Global::getDate(date, 24);
+   }
+   return num;
 }
