df787443737e9fef4b7f6186fd92229d45a00da1
Thomas Nipen <tnipen@gmail.com>
2014-10-22 23:47:39 +0200

    Generalize DownscalerElevation to other variables

--- src/Downscalers/Elevation.cpp
+++ src/Downscalers/Elevation.cpp
@@ -8,7 +8,8 @@ DownscalerElevation::DownscalerElevation(const Options& iOptions) : Downscaler(i
       mLapseRate(6.5),
       mComputeLapseRate(false),
       mShowLapseRate(false),
-      mType(typeTemperature) {
+      mType(typeTemperature),
+      mMaxLapseRate(Global::MV) {
    std::string neighbourhoodTag;
    //! Which neighbourhood scheme should be used to define the neighbourhood? Defaults to 1 nearest
    //! neighbour.
@@ -36,11 +37,17 @@ DownscalerElevation::DownscalerElevation(const Options& iOptions) : Downscaler(i
       mType = typeTemperature;
    else if(type == "pressure")
       mType = typePressure;
+   else if(type == "precipitation")
+      mType = typePressure;
+   else if(type == "windSpeed")
+      mType = typeWindSpeed;
    else {
       std::stringstream ss;
-      ss << "type must be one of: 'temperature', 'pressure'";
+      ss << "type must be one of: 'temperature', 'pressure', 'precipitation', 'windSpeed";
       Global::logger->write(ss.str(), Logger::error);
    }
+
+   iOptions.getValue("maxLapseRate", mMaxLapseRate);
    iOptions.check();
 }
 
@@ -51,14 +58,34 @@ float DownscalerElevation::downscale(const Input* iInput,
       const std::string& iVariable) const {
 
    std::vector<Location> locations = mNeighbourhood->select(iInput, iLocation);
+   std::vector<Location> nns;
+   iInput->getSurroundingLocations(iLocation, nns,1);
+   Location neighbour = nns[0];
 
    float value = Global::MV;
    float desiredElevation = iLocation.getElev();
+   float nearestT    = iInput->getValue(iDate, iInit, iOffset, neighbour.getId(), iMemberId, iVariable);
+   float nearestElev = neighbour.getElev();
 
    if(!Global::isValid(desiredElevation))
       return Global::MV;
 
-   if(mType == typeTemperature) {
+   if(mType == typePressure) {
+      int counter = 0;
+      float total = 0;
+      for(int i = 0; i < locations.size(); i++) {
+         float currElevation = locations[i].getElev();
+         float currValue     = iInput->getValue(iDate, iInit, iOffset, locations[i].getId(), iMemberId, iVariable);
+         if(Global::isValid(currValue)) {
+            counter++;
+            float adjustedValue = currValue * exp(-1.21e-4 * (desiredElevation - currElevation));
+            total += adjustedValue;
+         }
+      }
+      if(counter > 0)
+         value = total / counter;
+   }
+   else {
       // Find temperature by using linear regression on the elevation and temperatures
       // of neighbouring points
       if(mComputeLapseRate) {
@@ -67,8 +94,6 @@ float DownscalerElevation::downscale(const Input* iInput,
          float meanY   = 0; // T
          float meanXX  = 0; // elev*elev
          int   counter = 0;
-         float nearestT    = Global::MV;
-         float nearestElev = Global::MV;
          for(int i = 0; i < locations.size(); i++) {
             float x = locations[i].getElev();
             float y = iInput->getValue(iDate, iInit, iOffset, locations[i].getId(), iMemberId, iVariable);
@@ -77,11 +102,6 @@ float DownscalerElevation::downscale(const Input* iInput,
                meanX  += x;
                meanY  += y;
                meanXX += x*x;
-               // Store the temperature at the nearest valid neighbour
-               if(!Global::isValid(nearestT)) {
-                  nearestT = y;
-                  nearestElev = x;
-               }
                counter++;
             }
          }
@@ -90,7 +110,7 @@ float DownscalerElevation::downscale(const Input* iInput,
             value = Global::MV;
          }
          else {
-            float lapseRate; // In degrees / km
+            float lapseRate; // In units / km
             // If all points are at the same elevation, use the standard lapse rate
             if(fabs(meanXX - meanX*meanX) < 1) {
                lapseRate = mLapseRate;
@@ -103,10 +123,10 @@ float DownscalerElevation::downscale(const Input* iInput,
                meanXX /= counter;
                lapseRate = -(meanXY - meanX*meanY)/(meanXX - meanX*meanX)*1000;
                // Check against unreasonable lapse rates
-               if(lapseRate < -10.0)
-                  lapseRate = -10.0; // extreme unstable lapse rates
-               if(lapseRate > 10.0)
-                  lapseRate = 10.0; // extreme stable lapse rates
+               if(lapseRate < -mMaxLapseRate)
+                  lapseRate = -mMaxLapseRate; // extreme unstable lapse rates
+               if(lapseRate > mMaxLapseRate)
+                  lapseRate = mMaxLapseRate; // extreme stable lapse rates
             }
 
             if(mShowLapseRate) {
@@ -140,81 +160,6 @@ float DownscalerElevation::downscale(const Input* iInput,
             if(counter > 0)
                value = total / counter;
          }
-      }
-   }
-   else if(mType == typePressure) {
-      int counter = 0;
-      float total = 0;
-      for(int i = 0; i < locations.size(); i++) {
-         float currElevation = locations[i].getElev();
-         float currValue     = iInput->getValue(iDate, iInit, iOffset, locations[i].getId(), iMemberId, iVariable);
-         if(Global::isValid(currValue)) {
-            counter++;
-            float adjustedValue = currValue * exp(-1.21e-4 * (desiredElevation - currElevation));
-            total += adjustedValue;
-         }
-      }
-      if(counter > 0)
-         value = total / counter;
-   }
-   else {
-      // Find precip by using linear regression on the elevation and precip
-      // of neighbouring points
-      float meanXY  = 0; // elev*Precip
-      float meanX   = 0; // elev
-      float meanY   = 0; // Precip
-      float meanXX  = 0; // elev*elev
-      int   counter = 0;
-      float nearestP    = Global::MV;
-      float nearestElev = Global::MV;
-      for(int i = 0; i < locations.size(); i++) {
-         float x = locations[i].getElev();
-         float y = iInput->getValue(iDate, iInit, iOffset, locations[i].getId(), iMemberId, iVariable);
-         if(Global::isValid(x) && Global::isValid(y)) {
-            meanXY += x*y;
-            meanX  += x;
-            meanY  += y;
-            meanXX += x*x;
-            // Store the precip at the nearest valid neighbour
-            if(!Global::isValid(nearestP)) {
-               nearestP = y;
-               nearestElev = x;
-            }
-            counter++;
-         }
-      }
-      if(counter == 0) {
-         // No valid elevations or forecasts
-         value = Global::MV;
-      }
-      else {
-         float lapseRate; // In mm / km
-         // If all points are at the same elevation, then don't increase precip with height
-         if(fabs(meanXX - meanX*meanX) < 1) {
-            lapseRate = 0;
-         }
-         else {
-            // Estimate precip elevation rate
-            meanXY /= counter;
-            meanX  /= counter;
-            meanY  /= counter;
-            meanXX /= counter;
-            lapseRate = -(meanXY - meanX*meanY)/(meanXX - meanX*meanX)*1000;
-            // Check against unreasonable rates
-            if(lapseRate > 0)
-               lapseRate = 0; // Precip decreased with height
-            if(lapseRate < -3.0)
-               lapseRate = -3.0; // Precip increases too much with height
-         }
-
-         if(mShowLapseRate) {
-            // For debugging purposes only
-            value = lapseRate;
-         }
-         else {
-            value = moveParcel(nearestP, nearestElev, desiredElevation, lapseRate);
-         }
-         assert(Global::isValid(value));
       }
    }
    return value;
