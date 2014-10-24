35239aafd6a885648bbbc955fe81e1feda3c0d80
Thomas Nipen <tnipen@gmail.com>
2014-10-24 13:17:27 +0200

    Centering of points in CorrectorConditional

--- src/Correctors/Conditional.cpp
+++ src/Correctors/Conditional.cpp
@@ -4,11 +4,17 @@
 
 CorrectorConditional::CorrectorConditional(const Options& iOptions, const Data& iData) :
       Corrector(iOptions, iData) {
+   //! Edges between correction bins
    iOptions.getRequiredValues("edges", mEdges);
 
    std::string interpolatorTag;
-   iOptions.getRequiredValue("interpolator", interpolatorTag);
-   mInterpolator = Interpolator::getScheme(interpolatorTag);
+   //! How should the correction points be interpolated?
+   if(iOptions.getValue("interpolator", interpolatorTag)) {
+      mInterpolator = Interpolator::getScheme(interpolatorTag);
+   }
+   else {
+      mInterpolator = Interpolator::getScheme(Options("class=InterpolatorLinear"));
+   }
 
    for(int i = 1; i < mEdges.size(); i++) {
       mCenters.push_back((mEdges[i]+mEdges[i-1])/2);
@@ -21,59 +27,43 @@ void CorrectorConditional::correctCore(const Parameters& iParameters, Ensemble& 
    // Correct each ensemble member
    for(int n = 0; n < iUnCorrected.size(); n++) {
       float fcst = iUnCorrected[n];
-      if(Global::isValid(fcst) && fcst >=  mEdges[0] && fcst <= mEdges[mEdges.size()-1]) {
-         int lowerIndex = Global::MV;
-         int upperIndex = Global::MV;
-         // Find which bin the forecast is in
-         for(int i = 1; i < mEdges.size(); i++) {
-            if(fcst <= mEdges[i]) {
-               lowerIndex = i-1;
-               upperIndex = i;
-               break;
-            }
-         }
+      if(Global::isValid(fcst)) {
+         std::vector<float> param = iParameters.getAllParameters();
+         int B = param.size()/2;
+         std::vector<float> y(param.begin(), param.begin()+B); // mean observations
+         std::vector<float> x(param.begin()+B, param.end());   // mean forecasts
 
-         int B = iParameters.size();
-         assert(mCenters.size() == B);
-         if(fcst < mCenters[0]) {
-            iUnCorrected[n] = iParameters[0];
-         }
-         else if(fcst > mCenters[B-1]) {
-            iUnCorrected[n] = iParameters[B-1];
+         if(fcst >= x[0] and fcst <= x[B-1]) {
+            iUnCorrected[n] = mInterpolator->interpolate(fcst, x, y);
          }
          else {
-            for(int i = 1; i < mCenters.size(); i++) {
-               if(fcst < mCenters[i]) {
-                  // Interpolate
-                  float lowerFcst = mCenters[i-1];
-                  float upperFcst = mCenters[i];
-                  float lowerObs  = iParameters[i-1];
-                  float upperObs  = iParameters[i];
-                  if(lowerFcst == upperFcst) {
-                     iUnCorrected[n] = (lowerObs+upperObs)/2;
-                  }
-                  else {
-                     iUnCorrected[n] = lowerObs + (fcst - lowerFcst) * (upperObs-lowerObs)/(upperFcst - lowerFcst);
-                  }
-                  break;
-               }
-            }
+            // Don't correct forecasts outside the bins
          }
       }
    }
 }
 
 void CorrectorConditional::getDefaultParametersCore(Parameters& iParameters) const {
-   iParameters.setAllParameters(mCenters);
+   // Store parameters as follows:
+   // meanObs, ..., meanObs, meanFcst, ..., meanFcst
+   std::vector<float> values;
+   for(int k = 0; k < 2; k++) {
+      for(int i = 0; i < mCenters.size(); i++) {
+         values.push_back(mCenters[i]);
+      }
+   }
+   iParameters.setAllParameters(values);
 }
 
 void CorrectorConditional::updateParametersCore(const std::vector<Ensemble>& iUnCorrected,
       const std::vector<Obs>& iObs,
       Parameters& iParameters) const {
    std::vector<float> param = iParameters.getAllParameters();
+   int B = param.size()/2;
 
-   std::vector<float> totalObs(iParameters.size(), 0);
-   std::vector<int> counter(iParameters.size(), 0);
+   std::vector<float> totalObs(B, 0);
+   std::vector<float> totalFcst(B, 0);
+   std::vector<int> counter(B, 0);
    for(int i = 0; i < iObs.size(); i++) {
       float obs = iObs[i].getValue();
       float fcst = iUnCorrected[i].getMoment(1);
@@ -82,16 +72,21 @@ void CorrectorConditional::updateParametersCore(const std::vector<Ensemble>& iUn
             if(fcst < mEdges[i]) {
                int bin = i-1;
                totalObs[bin] += obs;
+               totalFcst[bin] += fcst;
                counter[bin]++;
                break;
             }
          }
       }
    }
-   for(int bin = 0; bin < iParameters.size(); bin++) {
+   for(int bin = 0; bin < B; bin++) {
       if(counter[bin] > 0) {
-         float value = totalObs[bin]/counter[bin];
-         iParameters[bin] = Processor::combine(iParameters[bin], value);
+         float obsValue  = totalObs[bin]/counter[bin];
+         float fcstValue = totalFcst[bin]/counter[bin];
+         int obsI  = bin;
+         int fcstI = bin+B;
+         iParameters[obsI] = Processor::combine(iParameters[obsI], obsValue);
+         iParameters[fcstI] = Processor::combine(iParameters[fcstI], fcstValue);
       }
    }
 }
